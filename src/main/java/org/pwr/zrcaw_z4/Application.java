8762df2f0da9383039eb3d2b049af44dce3bdfd6
Krzysztof Pilarczyk <radiator0@o2.pl>
2020-11-02 17:48:03 +0100

    Added TablesService and creation of table on startup

--- src/main/java/org/pwr/zrcaw_z4/Application.java
+++ src/main/java/org/pwr/zrcaw_z4/Application.java
@@ -1,7 +1,10 @@
 package org.pwr.zrcaw_z4;
 
+import org.pwr.zrcaw_z4.services.TablesService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,6 +18,17 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void setupTable() {
+        String tableName = "Note";
+        String key = "uuid";
+
+        TablesService tablesService = new TablesService();
+        if(!tablesService.isTableCreated(tableName)){
+            tablesService.createTable(tableName, key);
+        }
     }
 
 }
